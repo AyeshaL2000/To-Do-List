c516c2c3cc41e955f8938094107c16e2da57ae29
AyeshaL2000 <lakmaliayesha83@gmail.com>
2025-06-24 01:53:39 +0530

    update

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,10 +2,11 @@ import React,{useState}from"react"
 
 function ToDoList(){
 
-    const [tasks, setTasks]= useState([]);
-    const [newTask,setnewTask]= useState("");
+    const [tasks, setTasks]= useState(["Eat Breakfast","Take a shower","Walk the dog"]);
+    const [newTask,setNewTask]= useState("");
 
     function handleInputChange(event){
+     setNewTask(event.target.value);
 
     }
 
@@ -32,9 +33,33 @@ function ToDoList(){
         <div>
             <input
                type="text"
-            />
-
-        </div>
+               placeholder="Enter a task..."
+               value={newTask}
+               onChange={handleInputChange}/>
+            <button
+                className="add-button"
+                onClick={addTask}>
+                Add
+            </button>
+            
+            </div>
+        <ol>
+             {tasks.map((task, index)=> (
+                <li key={index}>
+                    <span className="text">{task}</span>
+                    <button
+                    className="delete-button"
+                    onClick={() => deleteTask(index)}>
+                        Delete
+                        </button>
+                        <button
+                    className="move-button"
+                    onClick={() => moveTaskup(index)}>
+                        Delete
+                        </button>
+                   </li>
+            ))}
+        </ol>
         
         </div>);
 
